e293090eeb669698c17ac20a242b38614345ed1c
Jacob <36286032+Hircinus@users.noreply.github.com>
2020-05-12 20:54:58 -0400

    New and better features
    Completed a fuller navbar experience with some fixes and inbuilt dropdown menu
    Cleaned up JS
    Fixed minor issues

--- src/pixel.js
+++ src/pixel.js
@@ -11,34 +11,6 @@ for (i=0;i<navContent.classList.length;i++) {
     continue;
   }
 }
-function dropdown() {
-  if (hiddenNav == 0) {
-    navContent.classList.remove("show");
-    hiddenNav++;
-    if (altMenuBtn!==false) {
-      if (menuButton.dataset.src != "") {
-        ogImg = menuButton.src;
-        menuButton.src = menuButton.dataset.src;
-        menuButton.dataset.src = ogImg;
-      }
-      else {}
-    }
-    else {}
-  }
-  else {
-    navContent.classList.add("show");
-    hiddenNav--;
-    if (altMenuBtn!==false) {
-      if (menuButton.dataset.src != "") {
-        altImg = menuButton.src;
-        menuButton.src = menuButton.dataset.src;
-        menuButton.dataset.src = altImg;
-      }
-      else {}
-    }
-  }
-}
-
 const pixel = {
   modal: function(modal, cover) {
     var modal = document.getElementById(modal);
@@ -50,6 +22,28 @@ const pixel = {
     else {
       modal.style.display = "flex";
       cover.style.display = "block";
+    }
+  },
+  navbarExtend: function() {
+    if (hiddenNav == 0) {
+      navContent.classList.remove("show");
+      hiddenNav++;
+      if (menuButton.dataset.src != "") {
+        ogImg = menuButton.src;
+        menuButton.src = menuButton.dataset.src;
+        menuButton.dataset.src = ogImg;
+      }
+      else {}
+    }
+    else {
+      navContent.classList.add("show");
+      hiddenNav--;
+      if (menuButton.dataset.src != "") {
+        altImg = menuButton.src;
+        menuButton.src = menuButton.dataset.src;
+        menuButton.dataset.src = altImg;
+      }
+      else {}
     }
   }
 }
